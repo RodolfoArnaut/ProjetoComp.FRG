f809f5ce3f076492377a1646a180723280d0cd81
Filipe Eduardo <filipeeacosta@gmail.com>
2022-06-10 19:37:46 -0300

    Aperfeiçoamento no acesso das páginas pós formulário

--- php/contato.php
+++ php/contato.php
@@ -1,59 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=
-  , initial-scale=1.0" />
-    <link rel="stylesheet" href="../styles/stylePhp.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet" />
-    <title>Contato</title>
-</head>
-
-<body>
-    <header>
-        <a href="../home.html">
-            <h1 id="title">
-                FRG <br />
-                BARBER
-            </h1>
-        </a>
-
-        <hr />
-    </header>
-    <div class="menu">
-        <nav>
-            <ul>
-                <a href="../home.html" id="Inicio">
-                    <li>Início</li>
-                </a>
-                <a href="../sobre.html" id="Sobre">
-                    <li>Sobre</li>
-                </a>
-            </ul>
-        </nav>
-    </div>
-    <hr />
-
-    <!-- Cabeçalho do formulário de contato -->
-    <div class="contactForm">
-        <h1>Obrigado por entrar em contato conosco!</h1>
-        <p>
-            Agora por favor aguarde enquanto nossa equipe de atendimento analisa sua mensagem. <br />
-            Responderemos sua mensagem o mais rápido possível.
-        </p>
-    </div>
-
-    <a id="voltar-ao-topo" href="#">&#9651;</a>
-    <br />
-    <br />
-
-    <div id="copyright">
-        <hr />
-        <p>Filipe Eduardo, Rodolfo Arnaut e Gustavo Resende - copyright © 2022</p>
-    </div>
-
     <?php
 
     $hostname = "127.0.0.1:3307";
@@ -74,6 +18,8 @@
     $insert = "INSERT INTO `contato` (`id`, `Nome`, `email`, `assunto`, `mensagem`, `data_mensagem`) VALUES (NULL, '{$name}', '{$email}', '{$assunto}', '{$mensagem}', current_timestamp());";
     mysqli_query($mysqli, $insert); // executa a query
 
+    header("Location: ../obgContato.html");
+
     // enviar notificação email
     // $mailHeader = "De:" . $name . "<" . $email . ">\r\n";
     // $emailDestino = "montilho12@gmail.com";
@@ -83,7 +29,3 @@
 
     // echo "mensagem enviada!";
     ?>
-
-</body>
-
-</html>
--- php/inscricoes.php
+++ php/inscricoes.php
@@ -1,62 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=
-  , initial-scale=1.0" />
-    <link rel="stylesheet" href="../styles/stylePhp.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet" />
-    <title>Contato</title>
-</head>
-
-<body>
-    <header>
-        <a href="../home.html">
-            <h1 id="title">
-                FRG <br />
-                BARBER
-            </h1>
-        </a>
-
-        <hr />
-    </header>
-    <div class="menu">
-        <nav>
-            <ul>
-                <a href="../home.html" id="Inicio">
-                    <li>Início</li>
-                </a>
-                <a href="../sobre.html" id="Sobre">
-                    <li>Sobre</li>
-                </a>
-                <a href="../contato.html" id="Contato">
-                    <li>Contato</li>
-                </a>
-            </ul>
-        </nav>
-    </div>
-    <hr />
-
-    <!-- Cabeçalho do formulário de contato -->
-    <div class="contactForm">
-        <h1>Obrigado por se inscrever em nossa newsletter!</h1>
-        <p>
-            Sempre que houverem novidades e promoções você será o primeiro(a) a saber!
-        </p>
-    </div>
-
-    <a id="voltar-ao-topo" href="#">&#9651;</a>
-    <br />
-    <br />
-
-    <div id="copyright">
-        <hr />
-        <p>Filipe Eduardo, Rodolfo Arnaut e Gustavo Resende - copyright © 2022</p>
-    </div>
-
-
     <?php
 
     $hostname = "127.0.0.1:3307";
@@ -75,6 +16,8 @@
     $insert = "INSERT INTO `inscritos` (`id`, `Nome`, `email`, `data_inscricao`) VALUES (NULL, '{$name}', '{$email}', current_timestamp());";
     mysqli_query($mysqli, $insert); // executa a query
 
+    header("Location: ../obgInscrito.html");
+
     // enviar notificação email
     // $mailHeader = "De:" . $name . "<" . $email . ">\r\n";
     // $assunto = "Um novo cliente se inscreveu na sua newsletter!";
@@ -86,7 +29,3 @@
 
     // echo "mensagem enviada!";
     ?>
-
-</body>
-
-</html>
