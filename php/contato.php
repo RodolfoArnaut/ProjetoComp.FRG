5263f47179b865f0856e6070d5d9a669a6172d1d
Filipe Eduardo <filipeeacosta@gmail.com>
2022-06-18 20:41:25 -0300

    PHPMailer adicionado e configurado

--- php/contato.php
+++ php/contato.php
@@ -1,23 +1,63 @@
 <?php
-    include_once 'sql.php';
+include_once 'sql.php';
+require_once './src/PHPMailer.php';
+require_once './src/SMTP.php';
+require_once './src/Exception.php';
 
-    $name = mysqli_real_escape_string($mysqli, $_POST['name']);
-    $email = mysqli_real_escape_string($mysqli, $_POST['email']);
-    $assunto = mysqli_real_escape_string($mysqli, $_POST['assunto']);
-    $mensagem = mysqli_real_escape_string($mysqli, $_POST['mensagem']);
+$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+$assunto = mysqli_real_escape_string($mysqli, $_POST['assunto']);
+$mensagem = mysqli_real_escape_string($mysqli, $_POST['mensagem']);
 
-    $insert = "INSERT INTO `contato` (`id`, `Nome`, `email`, `assunto`, `mensagem`, `data_mensagem`) 
+date_default_timezone_set("America/Argentina/Buenos_Aires"); //mesmo fuso horário que Brasília
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+$insert = "INSERT INTO `contato` (`id`, `Nome`, `email`, `assunto`, `mensagem`, `data_mensagem`) 
     VALUES (NULL, '{$name}', '{$email}', '{$assunto}', '{$mensagem}', current_timestamp());";
 
-    mysqli_query($mysqli, $insert); // executa a query
+mysqli_query($mysqli, $insert); // executa a query
 
-    header("Location: ../obgContato.html");
 
-    // enviar notificação email
-    // $mailHeader = "De:" . $name . "<" . $email . ">\r\n";
-    // $emailDestino = "montilho12@gmail.com";
+//enviar notificação email
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    // mail($emailDestino, $assunto, $mensagem, $mailHeader)
-    //     or die("Error!");
+$mail = new PHPMailer(true);
 
-    // echo "mensagem enviada!";
+try {
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = 'barberfrg@gmail.com';
+    $mail->Password = 'wczvbmopdwogrbwq';
+    $mail->Port = 587;
+
+    $mail->setFrom($email);
+    $mail->addAddress('barberfrg@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject = $assunto;
+    $mail->Body = "
+  <html>
+  <p>Nova mensagem de um(a) cliente!<p>
+  <p><b>Nome do(a) cliente: </b>$name</p>
+  <p><b>E-mail: </b>$email</p>
+  <p><b>Mensagem: </b>$mensagem</p>
+  <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+  </html>";
+    $mail->AltBody = "O(A) cliente " . $name . " enviou a seguinte mensagem: " . $mensagem . "!";
+
+    if ($mail->send()) {
+        echo 'Email enviado com sucesso';
+    } else {
+        echo 'Email não enviado';
+    }
+} catch (Exception $e) {
+    echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
+
+header("Location: ../obgContato.html");
--- php/inscricoes.php
+++ php/inscricoes.php
@@ -1,24 +1,61 @@
     <?php
     include_once 'sql.php';
+    require_once './src/PHPMailer.php';
+    require_once './src/SMTP.php';
+    require_once './src/Exception.php';
 
     $name = mysqli_real_escape_string($mysqli, $_POST['name']);
     $email = mysqli_real_escape_string($mysqli, $_POST['email']);
+
+    date_default_timezone_set("America/Argentina/Buenos_Aires"); //mesmo fuso horário que Brasília
+    $data_envio = date('d/m/Y');
+    $hora_envio = date('H:i:s');
 
     $insert = "INSERT INTO `inscritos` (`id`, `Nome`, `email`, `data_inscricao`) 
     VALUES (NULL, '{$name}', '{$email}', current_timestamp());";
 
     mysqli_query($mysqli, $insert); // executa a query
 
-    header("Location: ../obgInscrito.html");
 
     // enviar notificação email
-    // $mailHeader = "De:" . $name . "<" . $email . ">\r\n";
-    // $assunto = "Um novo cliente se inscreveu na sua newsletter!";
-    // $mensagem = "O cliente " . $nome . "se inscreveu na newsletter do seu site!";
-    // $emailDestino = "montilho12@gmail.com";
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-    // mail($emailDestino, $assunto, $mensagem, $mailHeader)
-    //     or die("Error!");
+    $mail = new PHPMailer(true);
 
-    // echo "mensagem enviada!";
+    try {
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+        $mail->Username = 'barberfrg@gmail.com';
+        $mail->Password = 'wczvbmopdwogrbwq';
+        $mail->Port = 587;
+
+        $mail->setFrom($email);
+        $mail->addAddress('barberfrg@gmail.com');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Novo(a) cliente inscrito na newsletter FRG Barber!';
+        $mail->Body = "
+        <html>
+        <p>Um(a) novo(a) cliente se inscreveu na sua newsletter!<p>
+        <p><b>Nome do(a) cliente: </b>$name</p>
+        <p><b>E-mail: </b>$email</p>
+        <p>O(A) cliente se inscreveu no dia: <b>$data_envio</b> às <b>$hora_envio</b></p>
+        </html>";
+        $mail->AltBody = "O(A) cliente " . $name . " se inscreveu na newsletter FRG Barber!";
+
+        if ($mail->send()) {
+            echo 'Email enviado com sucesso';
+        } else {
+            echo 'Email não enviado';
+        }
+    } catch (Exception $e) {
+        echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+    }
+
+    header("Location: ../obgInscrito.html");
     ?>
