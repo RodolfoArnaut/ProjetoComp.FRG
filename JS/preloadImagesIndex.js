f4770e08cbe90bab4abe58a6a6f6a4c191ef34a6
Filipe Eduardo <filipeeacosta@gmail.com>
2022-06-22 23:22:52 -0300

    Preload de imagens com Javascript

---
+++ JS/preloadImagesIndex.js
@@ -0,0 +1,17 @@
+var images = new Array();
+
+function preloadImages() {
+  for (i = 0; i < preloadImages.arguments.length; i++) {
+    images[i] = new Image();
+
+    images[i].src = preloadImages.arguments[i];
+  }
+}
+
+preloadImages(
+  "../images/barber.png",
+  "../images/Fundo3.jpg",
+  "../images/img01.jpg",
+  "../images/img02.jpg",
+  "../images/img03.jpg"
+);
