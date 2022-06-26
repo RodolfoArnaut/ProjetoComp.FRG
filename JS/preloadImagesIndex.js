b1739fff722db863531cf16fb67b7ec628e5ea3a
Filipe Eduardo <filipeeacosta@gmail.com>
2022-06-26 20:54:15 -0300

    Imagens otimizadas, preload javascript atualizado

--- JS/preloadImagesIndex.js
+++ JS/preloadImagesIndex.js
@@ -9,10 +9,18 @@ function preloadImages() {
 }
 
 preloadImages(
-  "../images/barber.png",
   "../images/Fundo3.jpg",
+  "../images/barber.png",
   "../images/img01.jpg",
   "../images/img02.jpg",
   "../images/img03.jpg",
-  "../images/Logo.png"
+  "../images/Logo.png",
+  "../images/avaliacao01.png",
+  "../images/avaliacao02.png",
+  "../images/avaliacao03.png",
+  "../images/avaliacao04.png",
+  "../images/avaliacao05.png",
+  "../images/avaliacao06.png",
+  "../images/avaliacao07.png",
+  "../images/avaliacao08.png"
 );
